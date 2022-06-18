166d602b56a17a759af80ab27c3d4fabb0d7aad3
Sergey <47526333+Penton7@users.noreply.github.com>
2022-06-18 20:01:04 +0300

    Update install_and_run_sui.sh

--- install_and_run_sui.sh
+++ install_and_run_sui.sh
@@ -23,6 +23,10 @@ sudo apt-get update;
 
 checkDocker
 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
 cd $HOME
 
 git clone https://github.com/MystenLabs/sui.git
