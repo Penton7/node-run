0399b09debd90f7fa0653edd9572dfd59ed64470
penton7 <ontarget1212@gmail.com>
2022-06-18 10:59:06 +0300

    sui node

---
+++ install_and_run_sui.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+
+function checkDocker {
+  if systemctl --all --type service | grep -q "docker";then
+      echo "docker exists."
+  else
+      echo "docker does NOT exist."
+      sudo apt-get install -y docker.io;
+
+      sudo groupadd docker;
+
+      sudo usermod -aG docker $USER;
+
+      sudo chmod 666 /var/run/docker.sock
+
+      sudo systemctl restart docker
+  fi
+}
+
+. <(wget -qO- https://raw.githubusercontent.com/Penton7/node-run/main/logo.sh)
+
+sudo apt-get update;
+
+checkDocker
+
+cd $HOME
+
+mkdir sui && cd sui
+
+wget https://github.com/MystenLabs/sui/raw/main/crates/sui-config/data/fullnode-template.yaml
+
+wget https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
+
+docker-compose up -d
