e7d7c24aa738d52d18e9c83afed78794c81a789c
Sergey <47526333+Penton7@users.noreply.github.com>
2022-06-26 22:54:36 +0300

    Update install_and_run_gitopia.sh

--- install_and_run_gitopia.sh
+++ install_and_run_gitopia.sh
@@ -6,7 +6,16 @@ sudo rm -rf gitopia .gitopia
 
 sudo apt-get update
 
-sudo apt-get install -y make gcc golang-go
+sudo apt-get install -y make gcc
+
+ver="1.18.1" && \
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" && \
+sudo rm -rf /usr/local/go && \
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" && \
+rm "go$ver.linux-amd64.tar.gz" && \
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile && \
+source $HOME/.bash_profile && \
+go version
 
 mkdir -p $HOME/go/bin
 
