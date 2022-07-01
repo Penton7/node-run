99d87422afb2aec6644fb251f43a275fb8193101
Sergey <ontarget1212@gmail.com>
2022-07-01 20:34:06 +0300

    aptos update

--- install_and_run_aptos_ait2.sh
+++ install_and_run_aptos_ait2.sh
@@ -24,9 +24,9 @@ sudo apt-get install -y unzip;
 
 checkDocker
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
+curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 wget -qO $HOME/aptos.zip "https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-0.2.0/aptos-cli-0.2.0-Ubuntu-x86_64.zip"
 unzip $HOME/aptos.zip
@@ -57,9 +57,9 @@ key_pub=$(cat root_key.pub)
 key="0x"$key_pub
 
 echo "---
-root_key: "F22409A93D1CD12D2FC92B5F8EB84CDCD24C348E32B3E7A720F3D2E288E63394"
+root_key: \"F22409A93D1CD12D2FC92B5F8EB84CDCD24C348E32B3E7A720F3D2E288E63394\"
 users:
-  - "$node_name"
+  - \"$node_name\"
 chain_id: 40
 min_stake: 0
 max_stake: 100000
@@ -68,11 +68,12 @@ max_lockup_duration_secs: 2592000
 epoch_duration_secs: 86400
 initial_lockup_timestamp: 1656615600
 min_price_per_gas_unit: 1
-allow_new_validators: true" | tee layout.yaml
+allow_new_validators: true" >layout.yaml
 
 wget https://github.com/aptos-labs/aptos-core/releases/download/aptos-framework-v0.2.0/framework.zip
 unzip framework.zip
 
-aptos genesis generate-genesis --local-repository-dir ~/$WORKSPACE --output-dir ~/$WORKSPACE
+aptos genesis generate-genesis --assume-yes --local-repository-dir ~/$WORKSPACE --output-dir ~/$WORKSPACE
+sleep 2
 
 docker-compose up -d
