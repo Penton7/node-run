ba57ff78124c8b638d2ae2e54f3ba1a7ef4d6d25
Sergey <ontarget1212@gmail.com>
2022-07-01 18:37:38 +0300

    aptos ait2

--- install_and_run_aptos_ait2.sh
+++ install_and_run_aptos_ait2.sh
@@ -52,8 +52,8 @@ aptos genesis set-validator-configuration \
     --username $node_name \
     --validator-host $ip:6180
 
-aptos key generate --output-file ~/.aptos/root_key
-key_pub=$(cat ~/.aptos/root_key.pub)
+aptos key generate --output-file root_key
+key_pub=$(cat root_key.pub)
 key="0x"$key_pub
 
 echo "---
