fd7b06bbe5fcbbfba715a2bf27bb580cb00bae11
Sergey <ontarget1212@gmail.com>
2022-07-01 17:38:17 +0300

    aptos ait2

--- install_and_run_aptos_ait2.sh
+++ install_and_run_aptos_ait2.sh
@@ -53,11 +53,19 @@ aptos genesis set-validator-configuration \
 
 
 echo "---
-      root_key: "0x5243ca72b0766d9e9cbf2debf6153443b01a1e0e6d086c7ea206eaf6f8043956"
+      root_key: "F22409A93D1CD12D2FC92B5F8EB84CDCD24C348E32B3E7A720F3D2E288E63394"
       users:
         - $node_name
-      chain_id: 5" | tee layout.yaml
-
+      chain_id: 5
+      chain_id: 40
+      min_stake: 0
+      max_stake: 100000
+      min_lockup_duration_secs: 0
+      max_lockup_duration_secs: 2592000
+      epoch_duration_secs: 86400
+      initial_lockup_timestamp: 1656615600
+      min_price_per_gas_unit: 1
+      allow_new_validators: true" | tee layout.yaml
 
 wget https://github.com/aptos-labs/aptos-core/releases/download/aptos-framework-v0.2.0/framework.zip
 unzip framework.zip
