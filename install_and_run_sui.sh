e46b1647bc2a8c956c467ccf29846526fb62e42d
penton7 <ontarget1212@gmail.com>
2022-07-06 19:41:40 +0300

    update sui

--- install_and_run_sui.sh
+++ install_and_run_sui.sh
@@ -1,44 +1,52 @@
 #!/usr/bin/bash
 
-function checkDocker {
-  if systemctl --all --type service | grep -q "docker";then
-      echo "docker exists."
-  else
-      echo "docker does NOT exist."
-      sudo apt-get install -y docker.io;
-
-      sudo groupadd docker;
-
-      sudo usermod -aG docker $USER;
-
-      sudo chmod 666 /var/run/docker.sock
-
-      sudo systemctl restart docker
-  fi
-}
-
 . <(wget -qO- https://raw.githubusercontent.com/Penton7/node-run/main/logo.sh)
 
-sudo apt-get update;
+sudo apt update && sudo apt install curl -y < "/dev/null"
+bash_profile=$HOME/.bash_profile
+if [ -f "$bash_profile" ]; then
+    . $HOME/.bash_profile
+fi
 
-checkDocker
+apt-get update && DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get install -y --no-install-recommends tzdata git ca-certificates curl build-essential libssl-dev pkg-config libclang-dev cmake jq
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl https://sh.rustup.rs -sSf | sh -s -- -y
+source $HOME/.cargo/env
 
-sudo chmod +x /usr/local/bin/docker-compose
-
+rm -rf /var/sui/db /var/sui/genesis.blob
+mkdir -p /var/sui/db
 cd $HOME
-
 git clone https://github.com/MystenLabs/sui.git
+cd sui
+git remote add upstream https://github.com/MystenLabs/sui
+git fetch upstream
+git checkout --track upstream/devnet
+cp crates/sui-config/data/fullnode-template.yaml /var/sui/fullnode.yaml
+#curl -fLJO https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
+wget -O /var/sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
+sed -i.bak "s/db-path:.*/db-path: \"\/var\/sui\/db\"/ ; s/genesis-file-location:.*/genesis-file-location: \"\/var\/sui\/genesis.blob\"/" /var/sui/fullnode.yaml
+cargo build --release -p sui-node
+mv ~/sui/target/release/sui-node /usr/local/bin/
 
-cd sui/docker/fullnode
+echo "[Unit]
+Description=Sui Node
+After=network.target
 
-wget https://github.com/MystenLabs/sui/raw/main/crates/sui-config/data/fullnode-template.yaml
+[Service]
+User=$USER
+Type=simple
+ExecStart=/usr/local/bin/sui-node --config-path /var/sui/fullnode.yaml
+Restart=on-failure
+LimitNOFILE=65535
 
-wget https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
+[Install]
+WantedBy=multi-user.target" > $HOME/suid.service
 
-sed -i "s/3.9/3.3/g" docker-compose.yaml
-
-sed -i "s/127.0.0.1/0.0.0.0/g" fullnode-template.yaml
-
-docker-compose up -d
+mv $HOME/suid.service /etc/systemd/system/
+sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
+Storage=persistent
+EOF
+sudo systemctl restart systemd-journald
+sudo systemctl daemon-reload
+sudo systemctl enable suid
+sudo systemctl restart suid
