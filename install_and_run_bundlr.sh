6aeb41ea9ef9ef53aa00929ddfd710295af3701e
Sergey <ontarget1212@gmail.com>
2022-07-15 14:25:29 +0300

    bundlr node

---
+++ install_and_run_bundlr.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/bash
+. <(wget -qO- https://raw.githubusercontent.com/Penton7/node-run/main/logo.sh)
+
+function checkDocker {
+  if systemctl --all --type service | grep -q "docker";then
+      echo "docker exists."
+  else
+      echo "docker does NOT exist."
+      sudo apt-get install -y docker.io;
+
+      sudo groupadd docker;
+
+      sudo usermod -aG docker $USER;
+
+      sudo chmod 666 /var/run/docker.sock
+
+      sudo systemctl restart docker
+  fi
+}
+
+checkDocker
+
+curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+sudo apt update && sudo apt upgrade -y && \
+sudo apt install curl tar wget clang pkg-config libssl-dev libleveldb-dev jq build-essential bsdmainutils git make ncdu htop screen unzip bc fail2ban htop libpq-dev -y
+
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+cd $HOME
+git clone --recurse-submodules https://github.com/Bundlr-Network/validator-rust.git
+
+cd $HOME/validator-rust/
+
+cargo run --bin wallet-tool create > wallet.json
+
+echo '
+PORT=80
+BUNDLER_URL=https://testnet1.bundlr.network
+GW_CONTRACT="RkinCLBlY4L5GZFv8gCFcrygTyd5Xm91CzKlR6qxhKA"
+GW_ARWEAVE=https://arweave.testnet1.bundlr.network/
+' > .env
+
+docker-compose up -d
+
+cargo run --bin wallet-tool show-address --wallet ./wallet.json
+
+ip=$(wget -qO- eth0.me)
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+source ~/.bashrc
+
+nvm install 16
+
+nvm use 16
+
+#testnet-cli join RkinCLBlY4L5GZFv8gCFcrygTyd5Xm91CzKlR6qxhKA -w wallet.json -u http://$ip:80 -s 25000000000000
+
