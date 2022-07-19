417141ac4d8c2e39643860632d06e0c0e244ef5f
Sergey <ontarget1212@gmail.com>
2022-07-19 18:28:36 +0300

    update massa

--- install_and_run_massa.sh
+++ install_and_run_massa.sh
@@ -49,22 +49,6 @@ sudo systemctl daemon-reload
 #sudo cp $HOME/massa_backup/node_privkey.key $HOME/massa/massa-node/config/node_privkey.key
 open_ports
 
-cd $HOME/massa/massa-client/
-
-if [ -d "$HOME/massa_backup" ]
-     
-then
-    echo "Directory backup exists."
-    sudo cp $HOME/massa_backup/wallet.dat $HOME/massa/massa-client/wallet.dat
-    sudo cp $HOME/massa_backup/node_privkey.key $HOME/massa/massa-node/config/node_privkey.key
-else
-    ./massa-client wallet_generate_private_key
-    mkdir $HOME/massa_backup
-    sudo cp $HOME/massa/massa-client/wallet.dat $HOME/massa_backup/wallet.dat
-    sleep 30
-    sudo cp $HOME/massa/massa-node/config/node_privkey.key $HOME/massa_backup/node_privkey.key
-fi
-
 sudo systemctl restart massad
 
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/Massa/main/insert_variables.sh)
