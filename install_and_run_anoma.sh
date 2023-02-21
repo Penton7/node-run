ef30bc5380dc5dfca8b204b4fbbbd9132b6d5a93
penton7 <ontarget1212@gmail.com>
2023-02-21 10:56:23 +0100

    run anoma node

---
+++ install_and_run_anoma.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/bash
+. <(wget -qO- https://raw.githubusercontent.com/Penton7/node-run/main/logo.sh)
+
+sudo apt-get install -y make git-core libssl-dev pkg-config libclang-12-dev build-essential
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+export NAMADA_TAG=v0.13.4
+export TM_HASH=v0.1.4-abciplus
+
+git clone https://github.com/anoma/namada && cd namada && git checkout $NAMADA_TAG
+
+#build
+make build-release
+make install
+
+#install tendermint
+git clone https://github.com/heliaxdev/tendermint && cd tendermint && git checkout $TM_HASH
+make build
+
+namada --version
+
+export CHAIN_ID="public-testnet-3.0.81edd4d6eb6"
+namada client utils join-network --chain-id $CHAIN_ID
+
+sudo tee /etc/systemd/system/namada.service > /dev/null <<EOF
+[Unit]
+Description=Namada Node
+After=network.target
+
+[Service]
+Type=simple
+User=root
+WorkingDirectory=/root
+ExecStart=$(which namada) node ledger run
+Restart=always
+RestartSec=3
+LimitNOFILE=10000
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable namada
+sudo systemctl start namada
