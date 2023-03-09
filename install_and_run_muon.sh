203b6d2fbb6a28739a16c7850e06208bd605ee52
penton7 <ontarget1212@gmail.com>
2023-03-09 13:01:51 +0100

    node muon

---
+++ install_and_run_muon.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/bash
+function checkDocker {
+  if systemctl --all --type service | grep -q "docker";then
+      echo "docker exists."
+  else
+      echo "docker does NOT exist."
+      sudo apt-get install -y docker.io;
+
+      sudo groupadd docker;
+
+      sudo usermod -aG docker $USER;
+
+      sudo chmod 666 /var/run/docker.sock
+
+      sudo systemctl restart docker
+
+
+      curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+      sudo chmod +x /usr/local/bin/docker-compose
+      sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+  fi
+}
+
+. <(wget -qO- https://raw.githubusercontent.com/Penton7/node-run/main/logo.sh)
+
+sudo apt-get update;
+
+sudo apt-get install -y unzip;
+
+checkDocker
+
+cd $HOME
+
+mkdir muon-node  && cd muon-node
+
+curl -o docker-compose.yml https://raw.githubusercontent.com/muon-protocol/muon-node-js/testnet/docker-compose-pull.yml
+
+docker compose up -d
+
+echo http://$(wget -qO- eth0.me):8000/status
