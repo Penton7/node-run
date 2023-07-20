d2f865e7ae24f329532502dde664fd3969159d34
Sergey <ontarget1212@gmail.com>
2023-07-20 22:59:41 +0200

    fix taiko

--- install_and_run_taiko.sh
+++ install_and_run_taiko.sh
@@ -38,11 +38,15 @@ cp .env.sample .env
 
 read -p "Enter Private Key: " PRIVATE_KEY;
 
-#read -p "Enter Wallet Address: " FEE_ADDRESS;
+read -p "Enter Wallet Address: " FEE_ADDRESS;
 
 sed -i "s/ENABLE_PROVER=false/ENABLE_PROVER=true/g" .env
+sed -i "s/ENABLE_PROPOSER=false/ENABLE_PROPOSER=true/g" .env
+
 
 sed -i "s/L1_PROVER_PRIVATE_KEY=/L1_PROVER_PRIVATE_KEY=$PRIVATE_KEY/g" .env
+sed -i "s/L1_PROPOSER_PRIVATE_KEY=/L1_PROPOSER_PRIVATE_KEY=$PRIVATE_KEY/g" .env
+
 
 read -p "Enter Sepolia HTTP Endpoint: " L1_ENDPOINT_HTTP;
 read -p "Enter Sepolia WSS Endpoint: " L1_ENDPOINT_WS;
@@ -52,6 +56,6 @@ sed -i "s|L1_ENDPOINT_HTTP=|L1_ENDPOINT_HTTP=$L1_ENDPOINT_HTTP|g" .env
 sed -i "s|L1_ENDPOINT_WS=|L1_ENDPOINT_WS=$L1_ENDPOINT_WS|g" .env
 
 
-#sed "s/L2_SUGGESTED_FEE_RECIPIENT=/L2_SUGGESTED_FEE_RECIPIENT=$FEE_ADDRESS/g" -i .env.sample
+sed -i "s/L2_SUGGESTED_FEE_RECIPIENT=|L2_SUGGESTED_FEE_RECIPIENT=$FEE_ADDRESS|g" .env
 
 docker-compose up -d
