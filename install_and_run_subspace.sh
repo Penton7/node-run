0ad78e4b79d8f588fa0b1245400c587fa0eca577
Sergey <ontarget1212@gmail.com>
2023-08-07 13:35:52 +0200

    gemini 3e

--- install_and_run_subspace.sh
+++ install_and_run_subspace.sh
@@ -4,8 +4,8 @@
 
 cd $HOME
 rm -rf subspace*
-wget -O subspace-node https://github.com/subspace/subspace/releases/download/snapshot-2022-may-03/subspace-node-ubuntu-x86_64-snapshot-2022-may-03
-wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/snapshot-2022-may-03/subspace-farmer-ubuntu-x86_64-snapshot-2022-may-03
+wget -O subspace-node https://github.com/subspace/subspace/releases/download/gemini-3e-2023-jul-03/subspace-node-ubuntu-x86_64-v2-gemini-3e-2023-jul-03
+wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/gemini-3e-2023-jul-03/subspace-farmer-ubuntu-x86_64-v2-gemini-3e-2023-jul-03
 chmod +x subspace*
 mv subspace* /usr/local/bin/
 
@@ -25,7 +25,7 @@ After=network.target
 [Service]
 User=$USER
 Type=simple
-ExecStart=$(which subspace-node) --chain testnet --wasm-execution compiled --execution wasm --bootnodes \"/dns/farm-rpc.subspace.network/tcp/30333/p2p/12D3KooWPjMZuSYj35ehced2MTJFf95upwpHKgKUrFRfHwohzJXr\" --rpc-cors all --rpc-methods unsafe --ws-external --validator --telemetry-url \"wss://telemetry.polkadot.io/submit/ 1\" --telemetry-url \"wss://telemetry.subspace.network/submit 1\" --name $SUBSPACE_NODENAME
+ExecStart=$(which subspace-node) --chain gemini-3e --wasm-execution compiled --execution wasm --rpc-cors all --rpc-methods unsafe --ws-external --validator --telemetry-url \"wss://telemetry.polkadot.io/submit/ 1\" --telemetry-url \"wss://telemetry.subspace.network/submit 1\" --name $SUBSPACE_NODENAME
 Restart=on-failure
 LimitNOFILE=65535
 
@@ -40,7 +40,7 @@ After=network.target
 [Service]
 User=$USER
 Type=simple
-ExecStart=$(which subspace-farmer) farm --reward-address $SUBSPACE_WALLET --plot-size 20G
+ExecStart=$(which subspace-farmer) farm --disable-private-ips --reward-address $SUBSPACE_WALLET --plot-size 20G
 Restart=on-failure
 LimitNOFILE=65535
 
