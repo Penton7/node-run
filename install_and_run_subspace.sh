07d434eae8121b2403bf35d99a9d669de9ac04af
Sergey <ontarget1212@gmail.com>
2023-11-10 21:27:33 +0100

    update subspace

--- install_and_run_subspace.sh
+++ install_and_run_subspace.sh
@@ -9,6 +9,7 @@ wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/g
 chmod +x subspace*
 mv subspace* /usr/local/bin/
 
+rm -rf /home/subspace
 mkdir /home/subspace
 
 read -p "Enter Node Name: " SUBSPACE_NODENAME;
@@ -27,7 +28,7 @@ After=network.target
 [Service]
 User=$USER
 Type=simple
-ExecStart=$(which subspace-node) --chain gemini-3g --execution wasm --blocks-pruning 256 --state-pruning archive --no-private-ipv4 --validator --name $SUBSPACE_NODENAME
+ExecStart=$(which subspace-node) --chain gemini-3g --execution wasm --blocks-pruning 256 --state-pruning archive --validator --name $SUBSPACE_NODENAME
 Restart=on-failure
 LimitNOFILE=65535
 
