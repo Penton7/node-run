306eec8fa5ba77e73ae7bfb48690987b7ee34359
penton7 <ontarget1212@gmail.com>
2024-06-26 15:15:52 +0200

    allora and farcaster

---
+++ install_and_run_farcaster.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+function checkDocker {
+  if systemctl --all --type service | grep -q "docker";then
+      echo "docker exists."
+  else
+      echo "docker does NOT exist."
+      sudo apt-get install -y docker.io;
+
+      sudo groupadd docker;
+
+      sudo usermod -aG docker $USER;
+
+      sudo chmod 666 /var/run/docker.sock
+
+      sudo systemctl restart docker
+  fi
+}
+
+. <(wget -qO- https://raw.githubusercontent.com/Penton7/node-run/main/logo.sh)
+
+sudo apt-get update;
+
+sudo apt-get install -y unzip screen;
+
+checkDocker
+
+
+screen -S farfcaster
+
+curl -sSL https://download.thehubble.xyz/bootstrap.sh | bash
